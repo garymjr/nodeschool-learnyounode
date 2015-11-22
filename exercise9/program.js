f8f453c6bcdb8c9b94fe079f74b3a7e8d134f427
garymjr <garymjr@gmail.com>
2015-11-22 13:28:21 -0500

    refactored exercise9

--- exercise9/program.js
+++ exercise9/program.js
@@ -1,34 +1,31 @@
 var http = require('http')
 var bl = require('bl')
+var results = []
+var count = 0
 
-var url1, url2, url3
+function printResults() {
+	for (var i = 0; i < results.length; i++) {
+		console.log(results[i])
+	}
+}
 
-http.get(process.argv[2], function(res) {
-	res.pipe(bl(function(err, data) {
-		if (err) {
-			return console.err(err)
-		} else {
-			console.log(data.toString())
-			http.get(process.argv[3], function(res) {
-				res.pipe(bl(function(err, data) {
-					if (err) {
-						return console.err(err)
-					} else {
-						console.log(data.toString())
-						http.get(process.argv[4], function(res) {
-							res.pipe(bl(function(err, data) {
-								if (err) {
-									return console.err(err)
-								} else {
-									console.log(data.toString())
-								}
-							}))
-						})
+function httpGet(index) {
+	http.get(process.argv[2 + index], function(res) {
+		res.pipe(bl(function(err, data) {
+			if (err) {
+				return console.err(err)
+			} else {
+				results[index] = data.toString()
+				count++
 
-					}
-				}))
-			})
+				if (count == 3) {
+					printResults(results)
+				}
+			}
+		}))
+	})
+}
 
-		}
-	}))
-})
+for (var i = 0; i < 3; i++) {
+	httpGet(i)
+}
