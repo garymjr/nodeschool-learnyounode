b2b2a9dce0ca4919271d5207113453463c1eb01d
garymjr <garymjr@gmail.com>
2015-11-22 13:55:19 -0500

    exercise10

---
+++ exercise10/program.js
@@ -0,0 +1,8 @@
+var net = require('net')
+var strftime = require('strftime')
+
+server = net.createServer(function(socket) {
+	socket.end(strftime('%Y-%m-%d %H:%M\n'))
+})
+
+server.listen(process.argv[2])
